b93f93e2d71464f4257d98a50dc5dbcd210be37e
sdob <sdob@cs.ucc.ie>
2017-04-19 11:55:00 +0100

    Retry exactly once on failure

--- src/fetch-messages.js
+++ src/fetch-messages.js
@@ -1,10 +1,12 @@
 import $ from 'jquery';
 import axios from 'axios';
+import HTTP from 'http-status';
 import log from './log';
 import url from './urls';
 
-export default function fetchMessages() {
+export default function fetchMessages({ retry }) {
   log('Fetching Messages tab');
+
   // Make the request
   return axios.get(url)
     .then(handleResponse)
@@ -18,16 +20,23 @@ export default function fetchMessages() {
     const $received = $(data).find('#FeedMessagesWithInvitations');
 
     return $received.children('.feedmessage');
-    /*
-    */
   }
 
   // Handle errors
   function handleError(error) {
-    const { data } = error;
-    if (data) {
-      // We have a response from the server, so there's a network connection there
-      return console.error(data);
+    const { response } = error;
+    // We have a response from the server, so there's a network connection there
+    // and it's probably not our fault. FL returns 404s and 503s every now and then;
+    // we'll just reschedule a retry in a minute (if we're not retrying already)
+    if (response && !retry) {
+      if ([
+        HTTP.NOT_FOUND,
+        HTTP.SERVICE_UNAVAILABLE,
+      ].includes(response.status)) {
+        return setTimeout(() => fetchMessages({ retry: true }), 60 * 1000);
+      }
+      // Otherwise, we've done our best; we'll just wait for the next
+      // tick
     }
     return console.error(error);
   }
