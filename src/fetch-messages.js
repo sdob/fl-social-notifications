dfdaad7bbc1d5dcef4987afff8b84b5347443db7
sdob <sdob@cs.ucc.ie>
2017-04-20 14:05:30 +0100

    Bring 'retry' back into scope

--- src/fetch-messages.js
+++ src/fetch-messages.js
@@ -12,31 +12,31 @@ export default function fetchMessages(retry) {
   return axios.get(url)
     .then(handleResponse)
     .catch(handleError);
-}
 
-// Handle successes
-function handleResponse(response) {
-  const { data } = response;
-  // We're only looking for messages that have invitations
-  // (i.e., messages from another user to us)
-  const $received = $(data).find(`#${INBOX_ID}`);
+  // Handle successes
+  function handleResponse(response) {
+    const { data } = response;
+    // We're only looking for messages that have invitations
+    // (i.e., messages from another user to us)
+    const $received = $(data).find(`#${INBOX_ID}`);
 
-  return $received.children('.feedmessage');
-}
+    return $received.children('.feedmessage');
+  }
 
-function handleError(error) {
-  const { response } = error;
-  // If we have a response from the server, then we know we have a network connection,
-  // and it's probably not our fault. FL returns 404s and 503s every now and then;
-  // we'll just reschedule a retry in a minute, if we're not retrying already.
-  if (response) {
-    if (HTTP.NOT_FOUND === response.status || response.status >= 500) {
-      if (!retry) {
-        setTimeout(() => fetchMessages(true), 60 * 1000);
+  function handleError(error) {
+    const { response } = error;
+    // If we have a response from the server, then we know we have a network connection,
+    // and it's probably not our fault. FL returns 404s and 503s every now and then;
+    // we'll just reschedule a retry in a minute, if we're not retrying already.
+    if (response) {
+      if (HTTP.NOT_FOUND === response.status || response.status >= 500) {
+        if (!retry) {
+          setTimeout(() => fetchMessages(true), 60 * 1000);
+        }
+      } else {
+        // If we've received another kind of error, then we want it logged
+        console.error(error);
       }
-    } else {
-      // If we've received another kind of error, then we want it logged
-      console.error(error);
     }
   }
 }
